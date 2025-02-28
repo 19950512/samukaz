8a379c1d771d9b1b5563e5b1834b3a86a06378a4
19950512 <mattmaydana@gmail.com>
2025-02-28 08:33:33 -0300

    vai da bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Necessário para GitHub Pages
+  },
+  basePath: isProd ? "/samukaz" : "", // Usa "/samukaz" apenas em produção
+  assetPrefix: isProd ? "/samukaz" : "", // Prefixo para assets estáticos
 };
 
 export default nextConfig;
